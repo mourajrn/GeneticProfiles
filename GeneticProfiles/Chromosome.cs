0aaf3c01f35a71ef767cee1718c3822a991063d8
José de Moura <39865466+mourajrn@users.noreply.github.com>
2021-03-04 09:19:37 -0300

    Adicionar arquivos de projeto.

---
+++ GeneticProfiles/Chromosome.cs
@@ -0,0 +1,26 @@
+﻿using GeneticSharp.Domain.Chromosomes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticProfiles
+{
+    public class Chromosome : ChromosomeBase
+    {
+        public Chromosome(int length) : base(length)
+        {
+        }
+
+        public override IChromosome CreateNew()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Gene GenerateGene(int geneIndex)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
