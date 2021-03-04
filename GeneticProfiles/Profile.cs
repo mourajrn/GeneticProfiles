08bf66685e2cafa33057e906a661d525cae1773b
José de Moura <39865466+mourajrn@users.noreply.github.com>
2021-03-04 18:01:19 -0300

    Primeira versão do Profile

---
+++ GeneticProfiles/Profile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticProfiles
+{
+    public class Profile
+    {
+        public string Name { get; private set; }
+        public double Weight { get; private set; }
+    }
+}
