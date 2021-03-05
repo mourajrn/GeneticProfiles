20fe41491e06972180c662e594ad95852dd37fa4
José de Moura <mourajrn@gmail.com>
2021-03-04 22:07:13 -0300

    Definição de GeneValue

---
+++ GeneticProfiles/GeneValue.cs
@@ -0,0 +1,27 @@
+﻿using GeneticSharp.Domain.Randomizations;
+
+namespace GeneticProfiles
+{
+    public class GeneValue
+    {
+        public Span Span { get; private set; }
+        public Profile Profile { get; private set; }
+
+        public GeneValue(Span span)
+        {
+            Span = span;
+            Profile = Span.PossibleProfiles[RandomizationProvider.Current.GetInt(0, Span.PossibleProfiles.Count)];
+        }
+
+        public GeneValue(Span span, Profile profile)
+        {
+            Span = span;
+            Profile = profile;
+        }
+
+        public override string ToString()
+        {
+            return $"{Span.Lenght}m -> {Profile.Name}";
+        }
+    }
+}
