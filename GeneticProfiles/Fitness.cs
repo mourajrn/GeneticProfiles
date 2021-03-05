055eb9fdcd748750197e753d5e68871184f94266
José de Moura <mourajrn@gmail.com>
2021-03-04 22:08:54 -0300

    Função Fitness

---
+++ GeneticProfiles/Fitness.cs
@@ -0,0 +1,41 @@
+﻿using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Fitnesses;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeneticProfiles
+{
+    public class Fitness : IFitness
+    {
+        public double Evaluate(IChromosome chromosome)
+        {
+            List<GeneValue> genes = chromosome.GetGenes().Select(gene => gene.Value as GeneValue).ToList();
+
+            var result = genes
+                .GroupBy(gene => gene.Profile)
+                .Select(gene => new
+                {
+                    Profile = gene.Key,
+                    Total = gene.Sum(x => x.Span.Lenght)
+                })
+                .Select(r => new
+                {
+                    Profile = r.Profile,
+                    Total = r.Total.MultiploSuperior(6)
+                })
+                .ToList();
+
+            bool wrongProfile = false;
+            genes.ForEach(gene =>
+            {
+                if (!gene.Span.PossibleProfiles.Contains(gene.Profile))
+                    wrongProfile = true;
+            });
+
+            if (wrongProfile)
+                return (1 / result.Sum(r => r.Profile.Weight * r.Total)) * 0.01;
+
+            return 1 / result.Sum(r => r.Profile.Weight * r.Total);
+        }
+    }
+}
---
+++ GeneticProfiles/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticProfiles
+{
+    public static class Utils
+    {
+        public static double MultiploSuperior(this double valor, double multiplo) => Math.Ceiling(valor / multiplo) * multiplo;
+    }
+}
