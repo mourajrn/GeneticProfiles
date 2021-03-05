a57d36ac1b2d8d006bf9eab2a16b9616a61be260
José de Moura <mourajrn@gmail.com>
2021-03-04 22:04:17 -0300

    Redefinição de Profile

--- GeneticProfiles/Profile.cs
+++ GeneticProfiles/Profile.cs
@@ -10,5 +10,36 @@ namespace GeneticProfiles
     {
         public string Name { get; private set; }
         public double Weight { get; private set; }
+
+        public Profile(string name, double weight)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Weight = weight;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Name.Equals((obj as Profile).Name);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 19;
+
+                hash = hash * 31 + Name.GetHashCode();
+
+                return hash;
+            }
+        }
     }
 }
