0866060262d67ee702537d9b1b58d14aff272d32
José de Moura <mourajrn@gmail.com>
2021-03-04 22:04:30 -0300

    Definição de Span

---
+++ GeneticProfiles/Span.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticProfiles
+{
+    public class Span
+    {
+        public double Lenght { get; private set; }
+        public List<Profile> PossibleProfiles { get; private set; }
+
+        public Span(double lenght, List<Profile> possibleProfiles)
+        {
+            Lenght = lenght;
+            PossibleProfiles = possibleProfiles ?? throw new ArgumentNullException(nameof(possibleProfiles));
+        }
+
+        public override string ToString()
+        {
+            return Lenght.ToString();
+        }
+    }
+}
