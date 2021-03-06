9d17f890a32ef9f062c75d7036796598a6e913b5
José de Moura <mourajrn@gmail.com>
2021-03-06 20:29:36 -0300

    Leitura do arquivo de perfis

--- GeneticProfiles/Main.cs
+++ GeneticProfiles/Main.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,36 @@ namespace GeneticProfiles
 {
     public partial class Main : Form
     {
+        public static List<Profile> Profiles = new List<Profile>();
+
         public Main()
         {
             InitializeComponent();
+
+            try
+            {
+                using (var sr = new StreamReader("C:/Users/Pichau/Desktop/Perfis.csv"))
+                {
+                    string ln;
+
+                    NumberFormatInfo provider = new NumberFormatInfo();
+                    provider.NumberDecimalSeparator = ".";
+                    provider.NumberGroupSeparator = ",";
+
+                    while ((ln = sr.ReadLine()) != null)
+                    {
+                        string name = ln.Split(';')[0];
+                        double weight = Convert.ToDouble(ln.Split(';')[1], provider);
+
+                        Profiles.Add(new Profile(name, weight));
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
         }
 
         private void btnSolve_Click(object sender, EventArgs e)
