1c2b918dd54f59dd12dcf07cf73293e6addb64b5
José de Moura <mourajrn@gmail.com>
2021-03-06 20:55:29 -0300

    Design de AddSpans

--- GeneticProfiles/AddSpans.Designer.cs
+++ GeneticProfiles/AddSpans.Designer.cs
@@ -32,8 +32,12 @@
             this.textLength = new System.Windows.Forms.TextBox();
             this.labelTitle = new System.Windows.Forms.Label();
             this.labelLength = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dataGridProfiles = new System.Windows.Forms.DataGridView();
+            this.comboProfiles = new System.Windows.Forms.ComboBox();
+            this.labelProfiles = new System.Windows.Forms.Label();
+            this.buttonAssign = new System.Windows.Forms.Button();
+            this.buttonCreateSpan = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridProfiles)).BeginInit();
             this.SuspendLayout();
             // 
             // textTitle
@@ -42,7 +46,7 @@
             this.textTitle.Location = new System.Drawing.Point(17, 43);
             this.textTitle.Name = "textTitle";
             this.textTitle.Size = new System.Drawing.Size(112, 35);
-            this.textTitle.TabIndex = 0;
+            this.textTitle.TabIndex = 1;
             // 
             // textLength
             // 
@@ -50,7 +54,7 @@
             this.textLength.Location = new System.Drawing.Point(214, 43);
             this.textLength.Name = "textLength";
             this.textLength.Size = new System.Drawing.Size(112, 35);
-            this.textLength.TabIndex = 0;
+            this.textLength.TabIndex = 3;
             // 
             // labelTitle
             // 
@@ -59,7 +63,7 @@
             this.labelTitle.Location = new System.Drawing.Point(12, 12);
             this.labelTitle.Name = "labelTitle";
             this.labelTitle.Size = new System.Drawing.Size(54, 28);
-            this.labelTitle.TabIndex = 1;
+            this.labelTitle.TabIndex = 0;
             this.labelTitle.Text = "Title";
             // 
             // labelLength
@@ -69,32 +73,82 @@
             this.labelLength.Location = new System.Drawing.Point(209, 12);
             this.labelLength.Name = "labelLength";
             this.labelLength.Size = new System.Drawing.Size(77, 28);
-            this.labelLength.TabIndex = 1;
+            this.labelLength.TabIndex = 2;
             this.labelLength.Text = "Length";
             // 
-            // dataGridView1
+            // dataGridProfiles
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(17, 94);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(309, 305);
-            this.dataGridView1.TabIndex = 2;
+            this.dataGridProfiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridProfiles.Location = new System.Drawing.Point(17, 148);
+            this.dataGridProfiles.Name = "dataGridProfiles";
+            this.dataGridProfiles.RowHeadersWidth = 51;
+            this.dataGridProfiles.RowTemplate.Height = 24;
+            this.dataGridProfiles.Size = new System.Drawing.Size(309, 317);
+            this.dataGridProfiles.TabIndex = 7;
+            // 
+            // comboProfiles
+            // 
+            this.comboProfiles.Font = new System.Drawing.Font("Artifakt Element", 12F);
+            this.comboProfiles.FormattingEnabled = true;
+            this.comboProfiles.Location = new System.Drawing.Point(102, 93);
+            this.comboProfiles.Name = "comboProfiles";
+            this.comboProfiles.Size = new System.Drawing.Size(184, 36);
+            this.comboProfiles.TabIndex = 5;
+            // 
+            // labelProfiles
+            // 
+            this.labelProfiles.AutoSize = true;
+            this.labelProfiles.Font = new System.Drawing.Font("Artifakt Element", 12F);
+            this.labelProfiles.Location = new System.Drawing.Point(12, 96);
+            this.labelProfiles.Name = "labelProfiles";
+            this.labelProfiles.Size = new System.Drawing.Size(84, 28);
+            this.labelProfiles.TabIndex = 4;
+            this.labelProfiles.Text = "Profiles";
+            // 
+            // buttonAssign
+            // 
+            this.buttonAssign.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAssign.Font = new System.Drawing.Font("Artifakt Element", 12F);
+            this.buttonAssign.Location = new System.Drawing.Point(292, 93);
+            this.buttonAssign.Name = "buttonAssign";
+            this.buttonAssign.Size = new System.Drawing.Size(34, 36);
+            this.buttonAssign.TabIndex = 6;
+            this.buttonAssign.Text = "+";
+            this.buttonAssign.UseVisualStyleBackColor = true;
+            this.buttonAssign.Click += new System.EventHandler(this.buttonAssign_Click);
+            // 
+            // buttonCreateSpan
+            // 
+            this.buttonCreateSpan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCreateSpan.Font = new System.Drawing.Font("Artifakt Element", 12F);
+            this.buttonCreateSpan.Location = new System.Drawing.Point(17, 471);
+            this.buttonCreateSpan.Name = "buttonCreateSpan";
+            this.buttonCreateSpan.Size = new System.Drawing.Size(309, 36);
+            this.buttonCreateSpan.TabIndex = 8;
+            this.buttonCreateSpan.Text = "Create Span";
+            this.buttonCreateSpan.UseVisualStyleBackColor = true;
+            this.buttonCreateSpan.Click += new System.EventHandler(this.buttonCreateSpan_Click);
             // 
             // AddSpans
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(347, 411);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(347, 519);
+            this.Controls.Add(this.buttonCreateSpan);
+            this.Controls.Add(this.buttonAssign);
+            this.Controls.Add(this.comboProfiles);
+            this.Controls.Add(this.dataGridProfiles);
             this.Controls.Add(this.labelLength);
+            this.Controls.Add(this.labelProfiles);
             this.Controls.Add(this.labelTitle);
             this.Controls.Add(this.textLength);
             this.Controls.Add(this.textTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "AddSpans";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Create spans";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridProfiles)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,6 +160,10 @@
         private System.Windows.Forms.TextBox textLength;
         private System.Windows.Forms.Label labelTitle;
         private System.Windows.Forms.Label labelLength;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridProfiles;
+        private System.Windows.Forms.ComboBox comboProfiles;
+        private System.Windows.Forms.Label labelProfiles;
+        private System.Windows.Forms.Button buttonAssign;
+        private System.Windows.Forms.Button buttonCreateSpan;
     }
 }
--- GeneticProfiles/AddSpans.cs
+++ GeneticProfiles/AddSpans.cs
@@ -12,9 +12,33 @@ namespace GeneticProfiles
 {
     public partial class AddSpans : Form
     {
+        public List<Profile> PossibleProfiles { get; private set; }
+
         public AddSpans()
         {
             InitializeComponent();
+
+            PossibleProfiles = new List<Profile>();
+
+            foreach (Profile profile in Main.Profiles)
+            {
+                comboProfiles.Items.Add(profile);
+            }
+
+            dataGridProfiles.Columns.Add("Perfil", "Perfil");
+            dataGridProfiles.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+
+        private void buttonAssign_Click(object sender, EventArgs e)
+        {
+            PossibleProfiles.Add(comboProfiles.SelectedItem as Profile);
+            dataGridProfiles.Rows.Add(comboProfiles.SelectedItem);
+        }
+
+        private void buttonCreateSpan_Click(object sender, EventArgs e)
+        {
+            Main.Spans.Add(new Span(textTitle.Text, Convert.ToDouble(textLength.Text), PossibleProfiles));
+            Close();
         }
     }
 }
