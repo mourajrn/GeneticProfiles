a6aa018a1d18ac523ff205a717c789e4f1447171
José de Moura <mourajrn@gmail.com>
2021-03-06 22:39:39 -0300

    Retorno do método Solve

--- GeneticProfiles/Solver.cs
+++ GeneticProfiles/Solver.cs
@@ -1,4 +1,5 @@
 ﻿using GeneticSharp.Domain;
+using GeneticSharp.Domain.Chromosomes;
 using GeneticSharp.Domain.Crossovers;
 using GeneticSharp.Domain.Mutations;
 using GeneticSharp.Domain.Populations;
@@ -10,41 +11,13 @@ namespace GeneticProfiles
 {
     public class Solver
     {
-        public void Solve()
+        public Gene[] Solve()
         {
-            List<Span> spans = new List<Span>();
-
-            spans.Add(new Span(4, new List<Profile>
-            {
-                new Profile("22", 22),
-                new Profile("25", 25),
-            }));
-            spans.Add(new Span(5, new List<Profile>
-            {
-                new Profile("25", 25),
-            }));
-            spans.Add(new Span(3, new List<Profile>
-            {
-                new Profile("22", 22),
-                new Profile("25", 25),
-            }));
-            spans.Add(new Span(3, new List<Profile>
-            {
-                new Profile("22", 22),
-                new Profile("25", 25),
-            }));
-            spans.Add(new Span(2, new List<Profile>
-            {
-                new Profile("18", 18),
-                new Profile("22", 22),
-                new Profile("25", 25),
-            }));
-
             var selection = new RouletteWheelSelection();
             var crossover = new TwoPointCrossover();
             var mutation = new UniformMutation();
             var fitness = new Fitness();
-            var chromosome = new Chromosome(spans);
+            var chromosome = new Chromosome(Main.Spans);
             var population = new Population(1000, 1000, chromosome);
 
             var ga = new GeneticAlgorithm(population, fitness, selection, crossover, mutation);
@@ -54,6 +27,8 @@ namespace GeneticProfiles
             ga.MutationProbability = 0.5F;
 
             ga.Start();
+
+            return ga.BestChromosome.GetGenes();
         }
     }
 }
